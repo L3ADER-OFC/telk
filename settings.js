d2a525a364f560081d6f734add95c21c8193b4e2
L3AD3R-OFC <mildredtanyamashamba04@gmail.com>
2025-12-04 14:28:37 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,25 @@
+
+/*━━━━━━━━━━━━━━━━━━━━*/
+/** 
+* JoshKing 
+* MidKBAN
+* supreme
+*/
+/*━━━━━━━━━━━━━━━━━━━━*/
+
+const settings = {
+  packname: '',
+  author: '🄹🅾︎🅂🅷🄺🅸🄽🅶',
+  botName: "‎🅂🅸🄻🅴🄽🆃_🄻🅳🅃",
+  botOwner: 'MidKing', // Your name
+  ownerNumber: '263716729222', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.6.3",
+  updateZipUrl: "https://github.com/L3ADER-OFC/telk/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
