8606713d904cd04991583c82b3c2cbc1cefba4b9
L3AD3R-OFC <mildredtanyamashamba04@gmail.com>
2025-12-04 14:57:46 +0200

    Add files via upload

---
+++ commands/song.js
@@ -0,0 +1,94 @@
+
+//new song API 
+
+const yts = require('yt-search');
+const axios = require('axios');
+
+async function songCommand(sock, chatId, message) {
+    
+    
+    
+    try {
+         await sock.sendMessage(chatId, {
+            react: {
+                text: "🎵",
+                key: message.key
+            }
+        });
+        
+        const text = message.message?.conversation || message.message?.extendedTextMessage?.text;
+        const searchQuery = text.split(' ').slice(1).join(' ').trim();
+        
+        if (!searchQuery) {
+            return await sock.sendMessage(chatId, { 
+                text: "Urikuda nziyo yakadii yaurikuda kudownloader?"},{ quoted: message
+            });
+        }
+
+        // Search for the song
+        const { videos } = await yts(searchQuery);
+        if (!videos || videos.length === 0) {
+            return await sock.sendMessage(chatId, { 
+                text: "No songs found!"
+            });
+        }
+
+        
+
+        // Get the first video result
+        const video = videos[0];
+        const urlYt = video.url;
+
+        // Fetch audio data from API
+        const response = await axios.get(`https://api.privatezia.biz.id/api/downloader/ytmp3?url=${urlYt}`);
+        const apiData = response.data;
+
+        if (!apiData || !apiData.status || !apiData.result || !apiData.result.downloadUrl) {
+            return await sock.sendMessage(chatId, { 
+                text: "_Failed to fetch audio from the API. Please try again later._"},{ quoted: message
+            });
+        }
+
+        const audioUrl = apiData.result.downloadUrl;
+        const title = apiData.result.title;
+        
+       await sock.sendMessage(chatId, { text: `_🎶 Playing song: *${apiData.result.title}* 🎧_` }, { quoted: message });
+        //time out
+     //  const audioResponse = await axios({ method: "get", url: apiData.result.downloadUrl, responseType: "stream", timeout: 6000 });
+
+
+        
+        // Send the audio
+        await sock.sendMessage(chatId, {
+            audio: { url: audioUrl },
+            mimetype: "audio/mpeg",
+            fileName: `${title}.mp3`
+        }, { quoted: message });
+        
+        //successful react ✔️
+       await sock.sendMessage(chatId, { react: { text: '☠️', key: message.key } 
+        });
+       
+
+    } catch (error) {
+        console.error('Error in song2 command:', error);
+        await sock.sendMessage(chatId, { 
+            text: "Download failed. Please try again later."
+        });
+        
+        //err react ❌
+            await sock.sendMessage(chatId, {
+            react: { text: '❌', key: message.key }
+        });
+    }
+}
+
+module.exports = songCommand; 
+
+
+
+
+
+
+
+
